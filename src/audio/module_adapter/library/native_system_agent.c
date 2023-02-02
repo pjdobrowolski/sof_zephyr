be216c6604e327a0cd07f71668eab0022ec54864
Dobrowolski, PawelX <pawelx.dobrowolski@intel.com>
2023-02-02 15:30:56 +0100

    module_adapter: native_system_agent: Introduce call and interface
    This is part of fw which helps loaded module in communication with adsp
    Also it provides basic operations for creation of component driver
    
    Signed-off-by: Dobrowolski, PawelX <pawelx.dobrowolski@intel.com>

---
+++ src/audio/module_adapter/library/native_system_agent.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright(c) 2023 Intel Corporation. All rights reserved.
+ *
+ * Author: Pawel Dobrowolski <pawelx.dobrowolski@intel.com>
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <utilities/array.h>
+#include <native_system_agent.h>
+
+/* The create_instance_f is a function call type known in module. The module entry_point
+ * points to this type of function which starts module creation.
+ */
+
+typedef void* (*native_create_instance_f)(void *mod_cfg, void *parent_ppl,
+					  void **mod_ptr, void **mod_ops, void **mod_uuid);
+
+void *native_system_agent_start(uint32_t entry_point, uint32_t module_id, uint32_t instance_id,
+				uint32_t core_id, uint32_t log_handle, void *mod_cfg,
+				void *mod_interface, void *mod_uuid)
+{
+	struct native_system_agent native_sys_agent;
+
+	native_sys_agent.module_id = module_id;
+	native_sys_agent.instance_id = instance_id;
+	native_sys_agent.core_id = core_id;
+	native_sys_agent.log_handle = log_handle;
+	void *system_agent_p = &native_sys_agent;
+
+	native_create_instance_f ci = (native_create_instance_f)(entry_point);
+
+	return ci(mod_cfg, NULL, &system_agent_p, &mod_interface, &mod_uuid);
+}
